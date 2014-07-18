13e371d415e9c71c5c81f83db819a8fc221e4be2
saurabh <saurabh@schoolcom.in>
2014-07-18 11:39:33 +0530

    Install devise andd Add a User

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :statuses
-
+  
+  # devise requirement
   root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
